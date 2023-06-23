fa048517e484074df81a353eb55178ece2ca0735
lavacreeper <lavacreeper2333@gmail.com>
2023-06-23 16:49:39 +0800

    from mac

--- proxy_group/Applications.py
+++ proxy_group/Applications.py
@@ -1,6 +1,7 @@
-import re
+from ProxyGroups import ProxyGroup
+
 
 class Application:
-    def __init__(self, name, *countries):
+    def __init__(self, name, *countries: ProxyGroup):
         self.name = name
         self.countries = countries
--- proxy_group/ProxyGroups.py
+++ proxy_group/ProxyGroups.py
@@ -1,10 +1,17 @@
 import re
 
-class ProxyGroup:
-    def get_match_rules(self,match_rules,proxy_list):
-        match_rules = [re.compile(r) for r in match_rules]
-        return [p for p in proxy_list if any(r.match(p) for r in match_rules)]
 
-    def __init__(self,name,match_rules,proxy_list):
+class ProxyGroup:
+
+    def get_match_rules(self, match_rules, proxy_list):
+        result = []
+        for rule in match_rules:
+            if rule.startswith('!'):
+                result = [proxy for proxy in proxy_list if not re.search(rule[1:], proxy.name)]
+            else:
+                result = [proxy for proxy in proxy_list if re.search(rule, proxy.name)]
+        return result
+
+    def __init__(self, name, match_rules, proxy_dict:dict):
         self.name = name
-        self.proxy_list = self.get_match_rules(match_rules,proxy_list)
+        self.proxy_list = self.get_match_rules(match_rules, proxy_dict)
